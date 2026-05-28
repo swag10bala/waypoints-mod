9a37021e8200fb12acc1f838fb71e4b83bfb9be5
swag10bala <121708899+swag10bala@users.noreply.github.com>
2026-05-28 19:39:49 -0300

    Prepare v1.0.1 release

--- src/main/java/com/waypoints/WaypointsMod.java
+++ src/main/java/com/waypoints/WaypointsMod.java
@@ -9,7 +9,7 @@ import net.minecraftforge.fml.common.Mod.EventHandler;
 import net.minecraftforge.fml.common.event.FMLInitializationEvent;
 import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
 
-@Mod(modid = "waypoints", name = "Waypoints", version = "1.0.0", clientSideOnly = true, useMetadata = true)
+@Mod(modid = "waypoints", name = "Waypoints", version = "1.0.1", clientSideOnly = true, useMetadata = true)
 public class WaypointsMod {
    public static final String MODID = "waypoints";
    public static final WaypointManager WAYPOINTS = new WaypointManager();
